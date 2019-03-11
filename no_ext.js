9ab229c66e7b3a1f467feb61e0d25b358be35410
leshax <leshax@gmail.com>
2019-03-11 12:48:51 +0100

    First commit
    First commit

---
+++ no_ext.js
@@ -0,0 +1,45 @@
+
+function getNewItems(){
+	var items = document.querySelectorAll("#bets-listview .item");
+	var json = null;
+	for(var i=0; i<items.length; i++) {
+		
+		var item = items[i];
+		var idAttr = item.getAttribute("data-key");
+		if(localStorage.getItem(idAttr)) continue;
+		var percent = item.querySelector(".vperc").innerText;
+		var team1 = item.querySelector(".team-name1").innerText;
+		var team2 = item.querySelector(".team-name2").innerText;
+		var gameType = item.querySelector(".it-sp").innerText;
+		var time = item.querySelector(".vilkhr").innerText;
+		var b1 = item.querySelectorAll(".it-name")[0].innerText;
+		var b2 = item.querySelectorAll(".it-name")[1].innerText;
+		var jsonItem = { };
+		jsonItem["idAttr"] = idAttr;
+		jsonItem["percent"] = percent;
+		jsonItem["team1"] = team1;
+		jsonItem["team2"] = team2;
+		jsonItem["gameType"] = gameType;
+		jsonItem["time"] = time;
+		jsonItem["b1"] = b1;
+		jsonItem["b2"] = b2;
+		if(!json) json = {};
+		json[idAttr] = jsonItem;
+		localStorage.setItem(idAttr, jsonItem);
+	}
+	console.log(json);
+	return json;
+}
+function sendRequest(json) {	
+	if(!json){ return } 
+	var str = JSON.stringify(json);
+	console.log("str", str);	
+	var s = document.createElement("script");
+	s.src = "http://localhost:3000/" + str;
+	document.body.appendChild(s);
+}
+sendRequest(getNewItems());
+setTimeout(function() { location.reload() }, 10000);
+
+
+
---
+++ server.js
@@ -0,0 +1,73 @@
+const http = require('http');
+const https = require('https');
+var qs = require('querystring');
+var url  = require('url');
+var fs = require('fs');
+var telegramConfig = JSON.parse(fs.readFileSync('telegram_config.json', 'utf8'));
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+
+const server = http.createServer((req, res) => {
+
+	var json = createJson(req);
+	var msg = getMessageForTelegram(json);
+	var chatId = telegramConfig.chatId;
+	var botId = telegramConfig.botId;
+	var url = "https://api.telegram.org/bot"+botId+"/sendMessage?chat_id="+chatId+"&text="+msg;
+	
+	https.get(url, (resp) => { 
+		let data = '';
+		resp.on('data', (chunk) => {
+			data += chunk;
+		});
+		resp.on('end', () => {
+			console.log(JSON.parse(data).explanation);
+		});
+		resp.on("error", (err) => {
+			console.log("Error: " + err.message);
+		});
+		
+	});
+		
+	console.log("MSG:" + json);
+ 
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function getMessageForTelegram(json) {
+	try {
+		var message = "";
+		Object.keys(json).forEach(function(key,index) {
+			message += json[key].b1 + "/" + json[key].b2 + " " + json[key].gameType + ": %0A" + json[key].team1 + " vs " + json[key].team2 + " %0A" + json[key].percent+"%" + "%0A Last time updated: " + json[key].time + "; %0A%0A";
+		});		
+		console.log("message ", message); 
+		return message;
+	} catch (e){		
+		console.log("Error on creating telegram message");
+	}
+	
+}
+function createJson(req){
+	try{		
+		var str = url.parse(req.url).path.replace('/', "");
+		console.log("STR: " + str);	
+		var q = qs.unescape(str);
+		console.log("Q: " + q);
+		console.log("typeof: " + typeof q);
+		var json = JSON.parse(q);
+		console.log("json: " + json);
+		return json;
+	} catch(e) {
+		console.log(e);
+		console.log("Error parsing a json");
+	}
+}
